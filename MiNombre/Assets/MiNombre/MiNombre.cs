7208954442141964f587456a421f86af80e5aa4e
lete98 <joseluiscabezas@ceitartessos.com>
2019-10-21 20:20:12 +0200

    por fin tio

---
+++ MiNombre/Assets/MiNombre/MiNombre.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiNombre : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (" jose luis cabezas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (" 21");
+		
+	}
+}
